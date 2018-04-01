167d5031cd3c554e44b0b4052df5923ccbbf0c08
vit134 <vit134@mail.ru>
2018-04-01 11:54:16 +0300

    добавил новый роут, создал новую страницу для mvp поправил конфиг webpack

--- app/app.js
+++ app/app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
+var mvp = require('./routes/mvp');
 
 var app = express();
 
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use('/', index);
+app.use('/mvp', mvp);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+	entry: {
+		index: './app/views/pages/index/main.js',
+		mvp: './app/views/pages/mvp/main.js'
+	},
+	output: {
+		filename: '[name].js',
+		path: __dirname + '/app/dist/scripts'
+	},
 	module: {
 		rules: [
 			{
